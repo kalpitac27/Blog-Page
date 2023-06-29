c08bfd17846e4c471f53316d5f8df1101c443a08
Kalpita Chakraborty <122544321+kalpitac27@users.noreply.github.com>
2023-06-29 12:29:08 +0530

    Add files via upload

---
+++ project 1/js/main.js
@@ -0,0 +1,7 @@
+function callValue() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var query = document.getElementById("query");
+    document.writeln("Entered Information: " + "<br><br>" + "Your name: " + name + "<br>" + "Your email: " + email + "<br>" + "Your query: " + query);
+
+}
